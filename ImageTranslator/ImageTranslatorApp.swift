95190335de57bac3791bfb287f41fd7c2357d386
Marious Yousif <mariousyousif@oakland.edu>
2025-06-19 22:47:26 -0400

    Initial Commit

---
+++ ImageTranslator/ImageTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageTranslatorApp.swift
+//  ImageTranslator
+//
+//  Created by Marious Yousif on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
